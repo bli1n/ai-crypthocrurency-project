cae263cb95b901eb44d846d54aab1621bea9e473
bli1n <yongbin2003@gmail.com>
2022-05-18 14:36:42 +0900

    python code

---
+++ orderbook-collection.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+
+import datetime
+import json
+import csv
+import os
+import time
+import pandas as pd
+import requests
+import sys
+
+while(1):
+    book = {}
+    response = requests.get('https://api.bithumb.com/public/orderbook/BTC_KRW/?count=5')
+    book = response.json()
+
+    data = book['data']
+
+    bids = (pd.DataFrame(data['bids'])).apply(pd.to_numeric,errors='ignore')
+
+    bids.sort_values('price', ascending=False, inplace=True)
+    bids = bids.reset_index(); del bids['index']
+    bids['type'] = 0
+
+    asks = (pd.DataFrame(data['asks'])).apply(pd.to_numeric,errors='ignore')
+
+    asks.sort_values('price', ascending=True, inplace=True)
+    asks['type'] = 1
+    
+    df = bids.append(asks)
+    df['timestamp'] = datetime.datetime.now()
+
+    fn = 'test.csv'
+
+    header = os.path.exists(fn)
+    if header == False:
+        df.to_csv(fn, index=False, header=True, mode = 'a')
+    else:
+        df.to_csv(fn, index=False, header=False, mode = 'a')
+
